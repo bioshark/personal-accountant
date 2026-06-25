74da6db1da4399135da5a2e9a9be23cd8cbf7228
bioshark <rollandsov@gmail.com>
2026-06-25 21:54:55 +0200

    ideas

--- bin/pac.bat
+++ bin/pac.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIR=%~dp0..
 cd /d "%DIR%"
-java -Djava.rmi.server.hostname=localhost -jar "%DIR%\personal-accountant-0.0.1-SNAPSHOT.jar"
+java -Djava.rmi.server.hostname=localhost -jar "%DIR%\personal-accountant-1.0.0.jar"
--- bin/pac.sh
+++ bin/pac.sh
@@ -8,4 +8,4 @@ eval "$(jenv init -)"
 jenv shell 25
 
 java -Djava.rmi.server.hostname=localhost \
-     -jar "$DIR/personal-accountant-0.0.1-SNAPSHOT.jar"
+     -jar "$DIR/personal-accountant-1.0.0.jar"
